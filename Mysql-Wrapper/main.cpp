a779627096388989941b1ad538851655283cd6f3
unknown <Tomek@Tomek-Komputer.(none)>
2012-03-11 16:01:19 +0100

    Main file first definition

---
+++ Mysql-Wrapper/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Filename main.cpp
+ * 
+ * @author		Tomasz
+ * @date		2012-03-11
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	
+	system("PAUSE");
+	return 0;
+}
