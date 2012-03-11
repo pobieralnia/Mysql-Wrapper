666170cac59b813423604ae778e500ee4a983ad4
unknown <Tomek@Tomek-Komputer.(none)>
2012-03-11 18:25:37 +0100

    All headers included

--- Mysql-Wrapper/main.cpp
+++ Mysql-Wrapper/main.cpp
@@ -5,14 +5,25 @@
  * @date		2012-03-11
  */
 
+// System
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <Windows.h>	// For mysql.h to run on Windows
+#ifdef __LCC__ 
+#include <winsock.h>	// For windows if __LCC__ is defined (default - winsock.h is included in mysql.h)
+#endif 
+#include <mysql.h>
+
+// Addtional
+#include "dbWrapperClass.h"
 
 using namespace std;
 
 int main(void)
 {
+	DB_wrapper object;
+	object.init();
 	
 	system("PAUSE");
 	return 0;
